7f87ee9a813bad4df4efa31b8aa2fabdffb73f74
Krzysztof Kępiński <chris.kepinski@gmail.com>
2023-03-06 09:32:01 +0100

    fix: imports issue and cleanup

--- libs/setup.py
+++ libs/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dslibs',
-    version='0.0.6',
+    version='0.0.7',
     description='data science related tools & helpers',
     url='https://github.com/cjkepinsky/libs',
     author='Chris J. Kepinsky',
--- libs/src/dslibs/__init__.py
+++ libs/src/dslibs/__init__.py
@@ -1,2 +1,8 @@
-import cross_validation_score, mglearn, pdummies_score, simple_analyzer, simple_hyper_tuner \
-    , simple_processing, simple_reducer, simple_yahoo, simpleplotter, single_scoring
+from cross_validation_score import preprocess, separate_target, split_columns, split_train_test_80
+from simple_analyzer import print_analytics, print_simple_correlations
+from simple_hyper_tuner import quick_gridsearchcv_overview, gridsearchcv_tuner, predict_print_valid_scores, valid_score_tuner, print_valid_scores
+from simple_processing import fillna_val_cols, fillna_mean, fillna_val, dropna_rows, remove_columns, separate_target, split_train_test, categorical_numerical, categorize, categorize_train_valid_test, print_scores, get_model_name, normalize, normalize_cols
+from simple_reducer import umap_reducer, umap_reducer3d
+from simple_yahoo import get_from_yahoo
+from simpleplotter import feature_importance, chart, simple_roc, simple_confusion_matrix, simple_heatmap, simple_correlations, simple_features_overview
+from single_scoring import get_scoring
